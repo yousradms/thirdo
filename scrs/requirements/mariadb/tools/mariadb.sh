af897e36a44506d60c418c75ea233c4f1434b387
Yousra Doumas <ydoumas@e3r2p3.1337.ma>
2025-11-04 21:12:34 +0100

    d

--- scrs/requirements/mariadb/tools/mariadb.sh
+++ scrs/requirements/mariadb/tools/mariadb.sh
@@ -1,44 +1,32 @@
 #!/bin/sh
+set -e
 
-mysql_install_db
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-/etc/init.d/mysql start
+service mysql start
 
-#Check if the database exists
-
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
-then 
-
-	echo "Database already exists"
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
+    echo " Database already exists: $MYSQL_DATABASE"
 else
+    echo " Configuring MySQL..."
 
-# Set root option so that connexion without root password is not possible
+    mysql -uroot <<-EOSQL
+        ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
+        DELETE FROM mysql.user WHERE User='';
+        DROP DATABASE IF EXISTS test;
+        DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+        FLUSH PRIVILEGES;
 
-mysql_secure_installation << _EOF_
+        CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+        GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+        FLUSH PRIVILEGES;
+EOSQL
 
-Y
-root4life
-root4life
-Y
-n
-Y
-Y
-_EOF_
-
-#Add a root user on 127.0.0.1 to allow remote connexion 
-#Flush privileges allow to your sql tables to be updated automatically when you modify it
-#mysql -uroot launch mysql command line client
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Create database and user in the database for wordpress
-
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-
-#Import database in the mysql command line
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-
+    echo " MySQL initialized successfully."
 fi
 
-/etc/init.d/mysql stop
+service mysql stop
 
 exec "$@"
